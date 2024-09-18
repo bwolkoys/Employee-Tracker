cae23fcb49c67243d1bbb764683adec05330b038
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-09-18 12:50:50 -0700

    added prompts for employee tracker and answers

---
+++ server.js
@@ -0,0 +1,52 @@
+const inquirer = require('inquirer');
+
+function mainOptions() {
+    console.log("Welcome to the Employee Tracker");
+    inquirer
+        .prompt ([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'Please choose an option:', 
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                ]
+            }
+        ])
+        .then((answers) => {
+            switch (answers.action) {
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Exit':
+                    console.log("Exiting the application.");
+                    process.exit();
+            }
+        });
+};
+
+mainOptions();
